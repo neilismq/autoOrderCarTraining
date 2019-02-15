0e68a2bca39d7a99eb769d912c83d36e4fe73305
zhaozhiqiang <zhaozhiqiang@kingxunlian.com>
2019-02-15 12:01:09 +0800

    1

---
+++ src/main/java/com/bj/zzq/test/MyAspect.java
@@ -0,0 +1,18 @@
+package com.bj.zzq.utils;
+
+/**
+ * @Author: zhaozhiqiang
+ * @Date: 2019/2/13
+ * @Description:
+ */
+public class MyAspect {
+
+    public void before() {
+        System.out.println("鸡首");
+    }
+
+    public void after() {
+        System.out.println("牛后");
+    }
+
+}
---
+++ src/main/java/com/bj/zzq/test/UserService.java
@@ -0,0 +1,9 @@
+package com.bj.zzq.test;
+
+/**
+ * @Author: zhaozhiqiang
+ * @Date: 2019/2/13
+ * @Description:
+ */
+public class UserService {
+}
